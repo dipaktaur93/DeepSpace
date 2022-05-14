cd0855005d8c77c8d7a32ca0aa3df8d33f43bc6b
Dipak Taur <dipaktaur92@gmail.com>
2022-05-14 16:50:29 +0530

    Create Bubblesort.java

---
+++ Bubblesort.java
@@ -0,0 +1,22 @@
+public class Bubblesort {
+    public static void printArray(int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+    }
+    }
+             //bubble sort
+        public static void main (String[]args){
+            int[] arr = {7, 6, 8, 9, 1, 2, 5, 4, 3};
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = 0; j < arr.length - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        //swap
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            printArray(arr);
+        }
+    }
